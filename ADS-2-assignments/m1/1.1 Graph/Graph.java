0c3d934a6466f806fc9ebeafbe54b1675c32b722
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-30 00:59:54 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m1/1.1 Graph/Graph.java
+++ ADS-2-assignments/m1/1.1 Graph/Graph.java
@@ -1,217 +1,110 @@
-/******************************************************************************
- *  Compilation:  javac Graph.java        
- *  Execution:    java Graph input.txt
- *  Dependencies: Bag.java Stack.java In.java StdOut.java
- *  Data files:   https://algs4.cs.princeton.edu/41graph/tinyG.txt
- *                https://algs4.cs.princeton.edu/41graph/mediumG.txt
- *                https://algs4.cs.princeton.edu/41graph/largeG.txt
- *
- *  A graph, implemented using an array of sets.
- *  Parallel edges and self-loops allowed.
- *
- *  % java Graph tinyG.txt
- *  13 vertices, 13 edges 
- *  0: 6 2 1 5 
- *  1: 0 
- *  2: 0 
- *  3: 5 4 
- *  4: 5 6 3 
- *  5: 3 4 0 
- *  6: 0 4 
- *  7: 8 
- *  8: 7 
- *  9: 11 10 12 
- *  10: 9 
- *  11: 9 12 
- *  12: 11 9 
- *
- *  % java Graph mediumG.txt
- *  250 vertices, 1273 edges 
- *  0: 225 222 211 209 204 202 191 176 163 160 149 114 97 80 68 59 58 49 44 24 15 
- *  1: 220 203 200 194 189 164 150 130 107 72 
- *  2: 141 110 108 86 79 51 42 18 14 
- *  ...
- *  
- ******************************************************************************/
-
-import java.util.NoSuchElementException;
-
 /**
  * Class for graph.
  */
 public class Graph {
-    private static final String NEWLINE = System.getProperty("line.separator");
-
-    private final int V;
-    private int E;
-    private Bag<Integer>[] adj;
-    
     /**
-     * Initializes an empty graph with {@code V} vertices and 0 edges.
-     * param V the number of vertices
-     *
-     * @param  V number of vertices
-     * @throws IllegalArgumentException if {@code V < 0}
+     * vertex of type int.
      */
-    public Graph(int V) {
-        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
+    private final int vertexs;
+    /**
+     * edges of type int.
+     */
+    private int edges;
+    /**
+     * adj array of type Bag.
+     */
+    private Bag<Integer>[] adj;
+
+    /**
+     * Initializes an empty graph with {@code vertexs} vertices and 0 edges.
+     * param vertexs the number of vertices
+     *
+     * @param  vertexs1 number of vertices
+     * @throws IllegalArgumentException if {@code vertexs < 0}
+     */
+    public Graph(final int vertexs1) {
+        if (vertexs1 < 0) {
+            throw new IllegalArgumentException(
+                "Number of vertices must be nonnegative");
+        }
+        this.vertexs = vertexs1;
+        this.edges = 0;
+        adj = (Bag<Integer>[]) new Bag[vertexs1];
+        for (int v = 0; v < vertexs1; v++) {
             adj[v] = new Bag<Integer>();
         }
     }
 
-    // /**  
-    //  * Initializes a graph from the specified input stream.
-    //  * The format is the number of vertices <em>V</em>,
-    //  * followed by the number of edges <em>E</em>,
-    //  * followed by <em>E</em> pairs of vertices, with each entry separated by whitespace.
-    //  *
-    //  * @param  in the input stream
-    //  * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
-    //  * @throws IllegalArgumentException if the number of vertices or edges is negative
-    //  * @throws IllegalArgumentException if the input stream is in the wrong format
-    //  */
-    // public Graph(In in) {
-    //     try {
-    //         this.V = in.readInt();
-    //         if (V < 0) throw new IllegalArgumentException("number of vertices in a Graph must be nonnegative");
-    //         adj = (Bag<Integer>[]) new Bag[V];
-    //         for (int v = 0; v < V; v++) {
-    //             adj[v] = new Bag<Integer>();
-    //         }
-    //         int E = in.readInt();
-    //         if (E < 0) throw new IllegalArgumentException("number of edges in a Graph must be nonnegative");
-    //         for (int i = 0; i < E; i++) {
-    //             int v = in.readInt();
-    //             int w = in.readInt();
-    //             validateVertex(v);
-    //             validateVertex(w);
-    //             addEdge(v, w); 
-    //         }
-    //     }
-    //     catch (NoSuchElementException e) {
-    //         throw new IllegalArgumentException("invalid input format in Graph constructor", e);
-    //     }
-    // }
-
-
-    // /**
-    //  * Initializes a new graph that is a deep copy of {@code G}.
-    //  *
-    //  * @param  G the graph to copy
-    //  */
-    // public Graph(Graph G) {
-    //     this(G.V());
-    //     this.E = G.E();
-    //     for (int v = 0; v < G.V(); v++) {
-    //         // reverse so that adjacency list is in same order as original
-    //         Stack<Integer> reverse = new Stack<Integer>();
-    //         for (int w : G.adj[v]) {
-    //             reverse.push(w);
-    //         }
-    //         for (int w : reverse) {
-    //             adj[v].add(w);
-    //         }
-    //     }
-    // }
-
     /**
+     * the time complexity is O(1).
      * Returns the number of vertices in this graph.
      *
      * @return the number of vertices in this graph
      */
-    public int V() {
-        return V;
+    public int vertexs() {
+        return vertexs;
     }
 
     /**
+     * the time complexity is O(1).
      * Returns the number of edges in this graph.
      *
      * @return the number of edges in this graph
      */
-    public int E() {
-        return E;
-    }
-
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    public int edges() {
+        return edges;
     }
 
     /**
-     * Adds the undirected edge v-w to this graph.
+     * the time complexity is O(E).
+     * Adds the undirected edge verte-w to this graph.
      *
-     * @param  v one vertex in the edge
-     * @param  w the other vertex in the edge
-     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     * @param  v one vertexs in the edge
+     * @param  w the other vertexs in the edge
+     * @throws IllegalArgumentException unless.
      */
-    public void addEdge(int v, int w) {
-        validateVertex(v);
-        validateVertex(w);
-        E++;
+    public void addEdge(final int v, final int w) {
+        if (v == w) {
+            return;
+        }
+        if (!hasEdge(v, w)) {
+            edges++;
+        }
         adj[v].add(w);
         adj[w].add(v);
     }
 
 
     /**
-     * Returns the vertices adjacent to vertex {@code v}.
+     * the time complexity is O(1).
+     * Returns the vertices adjacent to vertexs {@code v}.
      *
-     * @param  v the vertex
-     * @return the vertices adjacent to vertex {@code v}, as an iterable
+     * @param  v the vertexs
+     * @return the vertices adjacent to vertexs {@code v}, as an iterable
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public Iterable<Integer> adj(int v) {
-        validateVertex(v);
+    public Iterable<Integer> adj(final int v) {
         return adj[v];
     }
-
     /**
-     * Returns the degree of vertex {@code v}.
+     * the time complexity is O(E).
+     * Determines if it has edge.
      *
-     * @param  v the vertex
-     * @return the degree of vertex {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     */
-    public int degree(int v) {
-        validateVertex(v);
-        return adj[v].size();
-    }
-
-
-    /**
-     * Returns a string representation of this graph.
+     * @param      v     first vertexs.
+     * @param      w     second vertexs.
      *
-     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
-     *         followed by the <em>V</em> adjacency lists
+     * @return     True if has edge, False otherwise.
      */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
-            s.append(v + ": ");
-            for (int w : adj[v]) {
-                s.append(w + " ");
+    public boolean hasEdge(final int v, final int w) {
+        boolean flag = false;
+        for (int i = 0; i < vertexs; i++) {
+            for (int k: adj[w]) {
+                if (v == k) {
+                    flag = true;
+                    break;
+                }
             }
-            s.append(NEWLINE);
         }
-        return s.toString();
+        return flag;
     }
-
-
-    // /**
-    //  * Unit tests the {@code Graph} data type.
-    //  *
-    //  * @param args the command-line arguments
-    //  */
-    // public static void main(String[] args) {
-    //     In in = new In(args[0]);
-    //     Graph G = new Graph(in);
-    //     StdOut.println(G);
-    // }
-
 }
--- ADS-2-assignments/m1/1.1 Graph/Solution.java
+++ ADS-2-assignments/m1/1.1 Graph/Solution.java
@@ -1,56 +1,68 @@
 import java.util.Scanner;
 import java.util.HashMap;
-// interface Graph {
-//     public int V();
-//     public int E();
-//     public void addEdge(int v, int w);
-//     public Iterable<Integer> adj(int v);
-//     public boolean hasEdge(int v, int w);
-// }
-
+/**
+ * Solution class.
+ */
 public final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
-
     }
-    public static void main(String[] args) {
+    /**
+     * main method is used for handling input.
+     *
+     * @param      args  The command line arguments.
+     */
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         String type = scan.nextLine();
         int vertex = Integer.parseInt(scan.nextLine());
         int edge = Integer.parseInt(scan.nextLine());
-        System.out.println(vertex + " vertices, " + edge + " edges ");
-        if(vertex == 0 || edge == 0) {
-            System.out.println("No edges");
-            return;
-        }
+
         Graph g = new Graph(vertex);
-        // System.out.println(vertex + " vertices");
-        // System.out.println(edge + " edges");
         String[] input = scan.nextLine().split(",");
         HashMap<Integer, String> map = new HashMap<Integer, String>();
 
-        for(int i = 0; i < vertex; i++) {
+        for (int i = 0; i < vertex; i++) {
             map.put(i, input[i]);
         }
 
-        for(int j = 0; j < edge; j++) {
+        for (int j = 0; j < edge; j++) {
             String[] edgeinput = scan.nextLine().split(" ");
-            g.addEdge(Integer.parseInt(edgeinput[0]), Integer.parseInt(edgeinput[1]));
+            g.addEdge(Integer.parseInt(
+                edgeinput[0]), Integer.parseInt(edgeinput[1]));
         }
 
-    StringBuilder s = new StringBuilder();
-    //s.append(vertex + " vertices, " + edge + " edges " + "\n");
-    for(int i = 0; i < vertex; i++) {
-        s.append(map.get(i) + ": ");
-        for(int j : g.adj(i)) {
-             s.append(map.get(j) + " ");
-            //System.out.println(j);
+        System.out.println(g.vertexs() + " vertices, " + g.edges() + " edges");
+        if (vertex <= 1) {
+            System.out.println("No edges");
+            return;
         }
-        s.append("\n");
-    }
 
-
-
-        System.out.println(s);
+        if (type.equals("List")) {
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < vertex; i++) {
+                s.append(map.get(i) + ": ");
+                for (int j : g.adj(i)) {
+                s.append(map.get(j) + " ");
+            }
+            s.append("\n");
+        }
+        System.out.println(s.toString().trim());
+        } else {
+            for (int i = 0; i < vertex; i++) {
+                for (int j = 0; j < vertex; j++) {
+                    if (g.hasEdge(i, j)) {
+                        System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+                }
+            System.out.println();
+            }
+        }
     }
 }
+
 
