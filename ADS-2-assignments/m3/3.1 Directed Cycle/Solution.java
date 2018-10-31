0a3d5d0b41cad129f2e567a2b44ec3bb683f7baf
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-31 20:13:44 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m3/3.1 Directed Cycle/Solution.java
+++ ADS-2-assignments/m3/3.1 Directed Cycle/Solution.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
  * Solution class.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-	}
-	/**
-	 * main method is used to hand the input.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertex = scan.nextInt();
-		int edges = scan.nextInt();
-		Digraph d = new Digraph(vertex);
-		for (int i = 0; i < edges; i++) {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main method is used to hand the input.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertex = scan.nextInt();
+        int edges = scan.nextInt();
+        Digraph d = new Digraph(vertex);
+        for (int i = 0; i < edges; i++) {
             d.addEdge(scan.nextInt(), scan.nextInt());
-		}
-		DirectedCycle dc = new DirectedCycle(d);
-		if(dc.hasCycle()) {
-			System.out.println("Cycle exists.");
-		} else {
-			System.out.println("Cycle doesn't exists.");
-		}
-	}
+        }
+        DirectedCycle dc = new DirectedCycle(d);
+        if (dc.hasCycle()) {
+            System.out.println("Cycle exists.");
+        } else {
+            System.out.println("Cycle doesn't exists.");
+        }
+    }
 }
