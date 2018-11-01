2d4cda3e080eb9b5b235613495e58b9492cd528b
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-11-01 20:56:26 +0530

    "ads2_m4_a1_1: 0/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -8,7 +8,7 @@ public final class Solution {
 	}
 	public static void main(String[] args) {
 		HashMap<Integer, ArrayList<String>> hash1 = new HashMap<Integer, ArrayList<String>>();
-		HashMap<Integer, String> hash2 = new HashMap<Integer, String>();
+		HashMap<String, ArrayList<Integer>> hash2 = new HashMap<String, ArrayList<Integer>>();
         Scanner scan = new Scanner(System.in);
         String file1 = scan.nextLine();
         String file2 = scan.nextLine();
@@ -18,19 +18,33 @@ public final class Solution {
         	content = new String(Files.readAllBytes(Paths.get("Files/" + file1)));
         	content1 = new String(Files.readAllBytes(Paths.get("Files/" + file2)));
 
-        	String[] st = content.split("\n");
-        	for(String s: st) {
+        	String[] input = content.split("\n");
+        	for(String s: input) {
         		String[] s1 = s.split(",");
         		for(int i = 1; i < s1.length-1; i++) {
-        			//System.out.println(s1[i]);
         			ArrayList<String> arr = new ArrayList<String>();
         			arr.add(s1[i]);
         			hash1.put(Integer.parseInt(s1[0]), arr);
         		}
         	}
         	for(Integer a: hash1.keySet()) {
-        		System.out.println(a + " " + hash1.get(a).toString().replace("[", "").replace("]","").trim());
+        		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+        		for(String n: hash1.get(a)) {
+        			arr1.add(a);
+        			hash2.put(n,arr1);
+        		}
+        		//System.out.println(a + " " + hash1.get(a).toString().replace("[", "").replace("]","").trim());
         	}
+        	for(String a: hash2.keySet()) {
+        		System.out.println(a + " " + hash2.get(a));
+        	}
+            Digraph d = new Digraph(hash2.size());
+        	String[] input1 = content1.split("\n");
+        	// for(String i1: input1) {
+        	// 	String[] s2 = i1.split(",");
+        	// 	d.addEdge(Integer.parseInt(s2[0]), Integer.parseInt(s2[1]));
+        	// }
+         //    System.out.println(d.toString());
         } catch(Exception e) {
         	System.out.println("No file");
         }
