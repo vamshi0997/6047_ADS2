89eb0b5654cf783e6a8008551f389f95b69e594e
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-11-06 21:16:48 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m8/1. Shortest Paths/Solution.java
+++ ADS-2-assignments/m8/1. Shortest Paths/Solution.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 import java.util.HashMap;
 /**
- * { item_description }
+ * Solution class.
  */
 public final class Solution {
-	/**
-	 * { item_description }
-	 */
+    /**
+     * default solution constructor.
+     */
     private Solution() {
 
     }
@@ -17,29 +17,31 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        HashMap<String, Integer> rail = new HashMap<String, Integer>();
+        HashMap<String, Integer> rail =
+        new HashMap<String, Integer>();
         String[] line = scan.nextLine().split(" ");
         int noofstations = Integer.parseInt(line[0]);
         int noofconnections = Integer.parseInt(line[1]);
         String[] stations = scan.nextLine().split(" ");
-        for(int k = 0; k < stations.length; k++) {
+        for (int k = 0; k < stations.length; k++) {
             rail.put(stations[k], k);
         }
 
         Edge e;
-        EdgeWeightedGraph ewg = new EdgeWeightedGraph(stations.length);
+        EdgeWeightedGraph ewg =
+        new EdgeWeightedGraph(stations.length);
         for (int i = 0; i < noofconnections; i++) {
-        	String[] elements = scan.nextLine().split(" ");
-        	e = new Edge(rail.get(elements[0]), rail.get(
-        		elements[1]), Double.parseDouble(elements[2]));
+            String[] elements = scan.nextLine().split(" ");
+            e = new Edge(rail.get(elements[0]), rail.get(
+                elements[1]), Double.parseDouble(elements[2]));
             ewg.addEdge(e);
         }
 
         int input = Integer.parseInt(scan.nextLine());
         DijkstraSP dk;
         for (int j = 0; j < input; j++) {
-        	String[] sd = scan.nextLine().split(" ");
-        	int source = rail.get(sd[0]);
+            String[] sd = scan.nextLine().split(" ");
+            int source = rail.get(sd[0]);
             dk = new DijkstraSP(ewg, source);
             System.out.println((int) dk.distance(rail.get(sd[1])));
         }
